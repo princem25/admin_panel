bc0281c56e36250f59a6872a53f371a8e5ec048c
princem25 <220170107066@vgecg.ac.in>
2026-03-26 15:33:10 +0530

    removed unwanted imports

--- admin/database/migrations/2026_03_25_122100_create_products_table.php
+++ admin/database/migrations/2026_03_25_122100_create_products_table.php
@@ -9,17 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
- public function up(): void
-{
-    Schema::create('products', function (Blueprint $table) {
-        $table->id();
-        $table->string('name');
-        $table->string('description')->nullable();
-        $table->decimal('price', 10, 2);
-        $table->string('image')->nullable();
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
