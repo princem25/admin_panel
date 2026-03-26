49b3e92aec3b0258122448bcccc19b771148cd28
princem25 <220170107066@vgecg.ac.in>
2026-03-26 15:36:56 +0530

    function for the filter in user and admin

--- admin/app/Models/Product.php
+++ admin/app/Models/Product.php
@@ -2,18 +2,36 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
- 
     protected $fillable = [
-    'name',
-    'category_id',
-    'description',
-    'price',
-    'image',
-    
-];
+        'name',
+        'category_id',
+        'description',
+        'price',
+        'image',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function scopeFilter(Builder $query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query->where('name', 'like', '%'.$search.'%');
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            $query->where('category_id', $category);
+        });
+
+        $query->when($filters['price'] ?? false, function ($query, $price) {
+            $query->where('price', '<=', $price);
+        });
+    }
 }
- 
