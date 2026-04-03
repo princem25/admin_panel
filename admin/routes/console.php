5a1cd1fa91fbce9042264fb1bd235a6dc0acd0ed
princem25 <220170107066@vgecg.ac.in>
2026-04-03 17:53:57 +0530

    command scheduling

--- admin/routes/console.php
+++ admin/routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+app(Schedule::class)
+    ->command('report:admin')
+    ->everyFourHours();
