4c4a6cec2d494a2f61de623d9dfdcd26d43dcf6b
princem25 <220170107066@vgecg.ac.in>
2026-04-07 11:28:35 +0530

    broadcasting config and env setup

--- admin/bootstrap/app.php
+++ admin/bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\RequestLoggingMiddleware;
 use App\Http\Middleware\roleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -9,13 +10,17 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
+        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-         $middleware->alias([
-        'role' => roleMiddleware::class,
-    ]);
-         $middleware->append(\App\Http\Middleware\RequestLoggingMiddleware::class);
+        $middleware->web(append: [
+            RequestLoggingMiddleware::class,
+        ]);
+        
+        $middleware->alias([
+            'role' => roleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- admin/resources/js/bootstrap.js
+++ admin/resources/js/bootstrap.js
@@ -1,4 +1,26 @@
 import axios from 'axios';
+import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
+import $ from 'jquery';
+
 window.axios = axios;
+window.$ = $;
+window.jQuery = $;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next, we will create an instance of Pusher as our broadcasting client.
+ * This is used to subscribe to channels and listen for events that are
+ * broadcast by Laravel. Echo abstracts all of this complexity for you.
+ */
+
+window.Pusher = Pusher;
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    useTLS: true,
+    encrypted: true,
+});
